ffddf63ba3b41ef7968a45d86628f133242c67ec
Lucas Denipoti <lucasfdenipoti@gmail.com>
2020-10-20 14:21:29 -0300

    início do programa

---
+++ POO_21_09_20/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace POO_21_09_20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

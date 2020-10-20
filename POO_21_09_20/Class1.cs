08f6c6b7ef6c60f02bf334c1bb9f8ed263e1e19f
Lucas Denipoti <lucasfdenipoti@gmail.com>
2020-10-20 15:29:32 -0300

    terminei o código

--- POO_21_09_20/Class1.cs
+++ POO_21_09_20/Class1.cs
@@ -1,8 +1,39 @@
 ﻿using System;
+using System.Globalization;
 
-public class Class1
-{
-	public Class1()
+public class Produto
+{ 
+	// declarando as variáveis
+	public string Nome;
+	public double Preço;
+	public int Quantidade;
+
+	// uso dos construtor
+	public Produto(string nome, double preço, int quantidade)
 	{
+		Nome = nome;
+		Preço = preço;
+		Quantidade = quantidade;
 	}
+
+	// declarar os métodos
+	public double ValorTotalEmEstoque() // indicar o valor total no estoque
+	{
+		return Preço * Quantidade;
+	}
+	public void AdicionarProdutos(int quantidade) // irá adicionar produtos da variável quantidade
+	{
+		Quantidade += quantidade;
+	} 
+	public void RemoverProdutos(int quantidade) // irá remover produtos da variável quantidade
+	{
+		Quantidade -= quantidade;
+	}
+
+	// declarar uma string para a saída dos produtos
+	public override string ToString()
+	{
+		return Nome + ", R$ " + Preço.ToString("F2", CultureInfo.InvariantCulture) + ", " + Quantidade + " unidades, Total: R$ " + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+	}
+
 }
